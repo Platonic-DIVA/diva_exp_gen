49c565f71d42eb70bdcff49ca0f776c7a018a575
thatrandomlurker-divamoddingtools <59970354+thatrandomlurker-divamoddingtools@users.noreply.github.com>
2020-07-08 21:26:13 -0230

    Now rounds the value to enable use in diva.

--- test.py
+++ test.py
@@ -2,11 +2,16 @@ with open("mmdcommands.csv") as mmd_expressions:
     with open("DivaDSC.txt") as DSC_Commands:
         for line in mmd_expressions:
             current_expression = line.split(",")
-            if current_expression[0] == "感嘆":
-                output_command = str("TIME(" + str(int(current_expression[1]) * 60) + "); \n" + "EXPRESSION(0, 5, " + str(float(current_expression[2]) * 100) + ", -1); ")
+            Name = current_expression[0]
+            Time = int(current_expression[1]) * 60
+            Value = float(current_expression[2]) * 100
+            
+            
+            if Name == "感嘆":
+                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 5, " + str(str(round(Value))) + ", -1); ")
                 
                 print(output_command)
             elif current_expression[0] == "ヤル":
-                output_command = str("TIME(" + str(int(current_expression[1]) * 60) + "); \n" + "EXPRESSION(0, 20, " + str(float(current_expression[2]) * 100) + ", -1); ")
+                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 20, " + str(str(round(Value))) + ", -1); ")
                 
                 print(output_command)
