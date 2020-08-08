f136d3f641e19ae40922c1bc493e7380ac98c449
thatrandomlurker-divamoddingtools <59970354+thatrandomlurker-divamoddingtools@users.noreply.github.com>
2020-08-08 13:33:34 -0230

    Update the script to YukiKami's version
    Update the script to include YukiKami's changes to the output

--- test.py
+++ test.py
@@ -1,103 +1,204 @@
 with open("mmdcommands.csv") as mmd_expressions:
-    with open("DivaDSC.txt") as DSC_Commands:
-        for line in mmd_expressions:
+    for line in mmd_expressions:
             current_expression = line.split(",")
             Name = current_expression[0]
-            Time = int(current_expression[1]) * 600
+            Time = float(current_expression[1]) / 60 * 100000
             Value = float(current_expression[2]) * 100
+            FrameValue = float(current_expression[2]) * 1000
             
-            if Name == "まばたき":
+            if Name == "まばたき": # Eye blink
                 if Value >= 50:
-                    output_command = str("TIME(" + str(Time) + "); \n" + "EYE_ANIM(0, 1, " + str(round(float(Time / (Value + 1)) / 10)) + "); ")
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EYE_ANIM(0, 1, " + "50" + "); ")
                     print(output_command)
                 if Value <= 50:
-                    output_command = str("TIME(" + str(Time) + "); \n" + "EYE_ANIM(0, 2, " + str(round(float(Time / (Value + 1)) / 1000)) + "); ")
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EYE_ANIM(0, 2, " + "100" + "); ")
                     print(output_command)
             if Name == "感嘆":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 5, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 5, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "ヤル":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 20, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
-            if Name == "笑い":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 1, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 20, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
+            if Name == "笑い": # Eyes - Happy
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 1, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "明確にする":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 11, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 11, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "クール":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 34, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 34, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "眩しい":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 8, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 8, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "元気":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 19, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 19, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "優しい":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 12, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 12, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "キリッ":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 15, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 15, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "くもん":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 36, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 36, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "くつう":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 37, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 37, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "ながし":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 13, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 13, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "なき":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 38, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 38, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "なやみ":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 39, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 39, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "考える":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 17, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 17, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "悲しい":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 0, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 0, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "せつな":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 18, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 18, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "アイスマイル":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 6, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 6, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "強い":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 10, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 10, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "ぴっくり２":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 40, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 40, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "ぴっくり":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 3, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
             if Name == "ウツロ":
-                output_command = str("TIME(" + str(Time) + "); \n" + "EXPRESSION(0, 16, " + str(str(round(Value))) + ", -1); ")
-                
-                print(output_command)
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 16, " + "50, " + str(str(round(FrameValue))) + ");")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "EXPRESSION(0, 3, " + "50" + ", 0);")
+                    print(output_command)
+            if Name == "あ": # Mouth_A
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 0, " + "100" + ", 1000);")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 8, " + "100" + ", 1000);")
+                    print(output_command)
+            if Name == "え": # Mouth_E
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 1, " + "100" + ", 1000);")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 8, " + "100" + ", 1000);")
+                    print(output_command)
+            if Name == "い": # Mouth_I
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 10, " + "100" + ", 1000);")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 8, " + "100" + ", 1000); ")
+                    print(output_command)
+            if Name == "お": # Mouth_O
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 2, " + "100" + ", 1000);")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 8, " + "100" + ", 1000);")
+                    print(output_command)
+            if Name == "う": # Mouth_U
+                if Value >= 1:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 11, " + "100" + ", 1000);")
+                    print(output_command)
+                if Value <= 0:
+                    output_command = str("TIME(" + str(int(Time)) + "); \n" + "MOUTH_ANIM(0, 0, 8, " + "100" + ", 1000);")
+                    print(output_command)
